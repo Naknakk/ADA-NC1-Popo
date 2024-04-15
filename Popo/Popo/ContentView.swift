53035f44302191d2abc95f84ea31e547a61bb37d
YunHak Lee <lyh990408@gmail.com>
2024-04-15 10:25:47 +0900

    [Setting] - 프로젝트 기초 세팅

---
+++ Popo/Popo/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Popo
+//
+//  Created by 이윤학 on 4/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Popo/Popo/SwiftData/ItemSD.swift
@@ -0,0 +1,18 @@
+////
+////  Item.swift
+////  Popo
+////
+////  Created by 이윤학 on 4/15/24.
+////
+//
+//import Foundation
+//import SwiftData
+//
+//@Model
+//final class Item {
+//    var timestamp: Date
+//    
+//    init(timestamp: Date) {
+//        self.timestamp = timestamp
+//    }
+//}
